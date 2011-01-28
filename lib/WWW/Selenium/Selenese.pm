9181b914ca8e33622b185f9e65005bd14516a421
Theodore Robert Campbell Jr <trcjr@cpan.org>
2011-01-28 05:20:02 +0000

    Import of TRCJR/WWW-Selenium-Selenese-0.002 from CPAN.
    gitpan-cpan-distribution: WWW-Selenium-Selenese
    gitpan-cpan-version:      0.002
    gitpan-cpan-path:         TRCJR/WWW-Selenium-Selenese-0.002.tar.gz
    gitpan-cpan-author:       TRCJR
    gitpan-cpan-maturity:     released

---
+++ lib/WWW/Selenium/Selenese.pm
@@ -0,0 +1,34 @@
+# ABSTRACT: turns baubles into trinkets
+use strict;
+use warnings;
+package WWW::Selenium::Selenese;
+BEGIN {
+  $WWW::Selenium::Selenese::VERSION = '0.002';
+}
+
+1;
+
+__END__
+=pod
+
+=head1 NAME
+
+WWW::Selenium::Selenese - turns baubles into trinkets
+
+=head1 VERSION
+
+version 0.002
+
+=head1 AUTHOR
+
+Theodore Robert Campbell Jr <trcjr@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2011 by Theodore Robert Campbell Jr.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
---
+++ t/00_compile.t
@@ -0,0 +1,9 @@
+use strict;
+use Test::More tests => 4;
+
+BEGIN {
+    use_ok 'WWW::Selenium::Selenese';
+    use_ok 'WWW::Selenium::Selenese::Command';
+    use_ok 'WWW::Selenium::Selenese::TestCase';
+    use_ok 'WWW::Selenium::Selenese::TestSuite';
+}
